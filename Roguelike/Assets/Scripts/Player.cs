2edaf80dcce327cc725e2b8d6ea6b5ffe9bb77cc
Brenton Wisniewski <bwisniewski@jvrenterprises.com>
2018-01-17 11:12:50 -0500

    start on player stuff

---
+++ Roguelike/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject {
+
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restarLevelDelay = 1f;
+
+	private Animator animator;
+	private int food;
+
+	// Use this for initialization
+	protected override void Start () {
+		animator = GetComponent<Animator> ();
+
+		food = GameManager.instance.playerFoodPoints;
+		base.Start ();
+	}
+
+	private void OnDisable() {
+		GameManager.instance.playerFoodPoints = food;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!GameManager.instance.playersTurn) return;
+	}
+
+	protected override void AttemptMove <T> (int xDir, int yDir) {
+		food--;
+
+		base.AttemptMove <T> (xDir, yDir);
+
+		RaycastHit2D hit;
+
+		CheckIfGameOver ();
+
+		GameManager.instance.playersTurn = false;
+	}
+
+	private void CheckIfGameOver() {
+		if (food <= 0) {
+			GameManager.instance.GameOver ();
+		}
+	}
+}
